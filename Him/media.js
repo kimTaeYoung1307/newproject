023b074af3cb5f3bfd23fcae5d578569bb52eb74
kimteayoung <52199518+kimteayoung@users.noreply.github.com>
2020-07-18 00:07:32 +0900

    add2

--- Him/media.js
+++ Him/media.js
@@ -2,6 +2,15 @@ const toogle = document.querySelector(".nav_toogle");
 const menu = document.querySelector(".nav_menu");
 const icon = document.querySelector(".nav_icon");
 
+function getApplyStyle(obj, prop) {
+  if (obj.currentStyle) {
+    var style = obj.currentStyle[prop];
+  } else if (window.getComputedStyle) {
+    var style = window.getComputedStyle(obj, null)[prop];
+  }
+  return style;
+}
+
 function init() {
   toogle.addEventListener("click", () => {
     menu.classList.toggle("active");
