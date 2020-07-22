aaf1ce8268000c2d4774f5c20c243e02b3413548
kimteayoung <52199518+kimteayoung@users.noreply.github.com>
2020-07-23 01:42:43 +0900

    end

--- Project/media.js
+++ Project/media.js
@@ -1,6 +1,21 @@
 const toogle = document.querySelector(".nav_toogle");
 const menu = document.querySelector(".nav_menu");
 const icon = document.querySelector(".nav_icon");
+const tmgsm = document.querySelector(".imgsm");
+
+let qws = 0;
+
+function imgturn() {
+  if (qws === 0) {
+    tmgsm.style.transition = "all 2s ease";
+    tmgsm.style.transform = `rotate(150deg)`;
+    qws = 1;
+  } else {
+    tmgsm.style.transition = "all 2s ease";
+    tmgsm.style.transform = `rotate(0deg)`;
+    qws = 0;
+  }
+}
 
 function getApplyStyle(obj, prop) {
   if (obj.currentStyle) {
@@ -16,6 +31,7 @@ function init() {
     menu.classList.toggle("active");
     icon.classList.toggle("active");
   });
+  setInterval(imgturn, 2000);
 }
 
 init();
